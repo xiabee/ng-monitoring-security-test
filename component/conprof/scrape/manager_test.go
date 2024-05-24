63f91abed8bb5e8458997ae4484d6cf59cbfc67b
xiabee <yunjie.xiao@pingcap.com.cn>
2024-05-24 16:12:16 +0800

    security test for release-7.5

--- component/conprof/scrape/manager_test.go
+++ component/conprof/scrape/manager_test.go
@@ -23,7 +23,6 @@ import (
 func TestMain(m *testing.M) {
 	opts := []goleak.Option{
 		goleak.IgnoreTopFunction("github.com/golang/glog.(*loggingT).flushDaemon"),
-		goleak.IgnoreTopFunction("github.com/golang/glog.(*fileSink).flushDaemon"),
 	}
 
 	goleak.VerifyTestMain(m, opts...)
